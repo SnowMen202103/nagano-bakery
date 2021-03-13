6030e3d730897cbced6ceea603827d484f1bd628
EC2 Default User <ec2-user@ip-172-31-52-252.ec2.internal>
2021-03-13 10:12:06 +0000

    コントローラー作成 genres deliverys order_details searches

---
+++ app/controllers/deliverys_controller.rb
@@ -0,0 +1,13 @@
+class DeliverysController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'searches/search'
+
+  get 'order_details/index'
+
+  get 'order_details/show'
+
+  get 'deliverys/index'
+
+  get 'deliverys/edit'
+
+  get 'deliverys/update'
+
+  get 'deliverys/destroy'
+
+  get 'genres/index'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'genres/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
