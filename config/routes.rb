f2f6df35e3b342f08d7076c6a402ffa2c2352bde
EC2 Default User <ec2-user@ip-172-31-75-85.ec2.internal>
2021-03-13 10:11:23 +0000

    add controller sweets carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'carts/index'
+
+  get 'sweets/index'
+
+  get 'sweets/show'
+
+  get 'sweets/new'
+
+  get 'sweets/edit'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
