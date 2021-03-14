2c37452167ea50fcf20ebf90d7a1bcb663e69110
uavpliyc <uavpliyc@gamil.com>
2021-03-14 05:27:02 +0000

    make view carts orders

--- config/routes.rb
+++ config/routes.rb
@@ -46,5 +46,6 @@ Rails.application.routes.draw do
 
   get 'orders/show'
 
+  get 'orders/thanks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
