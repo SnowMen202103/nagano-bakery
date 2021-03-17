cfccb55bc2a03b12b11ca1b3bd08ce9d1a9c3254
EC2 Default User <ec2-user@ip-172-31-52-252.ec2.internal>
2021-03-17 02:39:19 +0000

    finish cart

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,13 +1,12 @@
 class Customers::CartItemsController < ApplicationController
-  
+
   def index
     @cart_item = CartItem.where(customer_id:params[:customer_id])
     @customer = Customer.find(params[:customer_id])
-    
   end
 
   def update
-    cart_item = CartItem.find(params[:customer_id])
+    cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
     redirect_to request.referer
   end
@@ -29,5 +28,4 @@ class Customers::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:amount)
   end
-
 end
