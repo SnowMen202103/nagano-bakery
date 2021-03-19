1d84e8528ffd3d4369d67020fba8c4b7d1c2ad17
yskc0315 <ihcikihsoy1503@gmail.com>
2021-03-19 11:26:02 +0000

    okaki

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_03_19_110501) do
-ActiveRecord::Schema.define(version: 2021_03_19_101806) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -82,7 +81,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_101806) do
 
   create_table "order_details", force: :cascade do |t|
     t.integer "in_tax_price"
-    t.integer "sweet_number"
+    t.integer "amount"
     t.integer "making_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
