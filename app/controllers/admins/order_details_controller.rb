a59dfd98b98217620a3274a60d1ac0a39d0cbb42
uavpliyc <uavpliyc@gamil.com>
2021-03-20 16:11:12 +0000

    注文入金待ち、製作待ち

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -4,6 +4,14 @@ class Admins::OrderDetailsController < ApplicationController
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
     redirect_to admins_order_path(@order_detail.order)
+
+    # @order_details = @order_detail.order.order_details
+    # @order = Order.find(params[:id])
+    #   if @order_details.where(making_status: '製作中').any?
+    #     @order.update(order_status: 2)
+    #   else
+    #     @order_detail.update(order_detail_params)
+    #   end
   end
 
 
