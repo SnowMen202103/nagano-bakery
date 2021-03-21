51cf0d3d24e928b2dad23bf9b711e964a7d15ce0
uavpliyc <uavpliyc@gamil.com>
2021-03-21 03:23:38 +0000

    make status changes

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,17 +1,17 @@
 class Admins::OrderDetailsController < ApplicationController
 
   def update
-    @order_detail = OrderDetail.find(params[:id])
+    @order_detail         = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    @order_details        = @order_detail.order.order_details.all
+    @order                = Order.find(@order_detail.order_id)
+    @order_details_status = @order_details.pluck(:making_status)
+      if @order_details.exists?(making_status: '製作中')
+         @order.update(order_status: 2)
+      elsif @order_details_status.all?{|n| n == '製作完了'}
+         @order.update(order_status: 3)
+      end
     redirect_to admins_order_path(@order_detail.order)
-
-    # @order_details = @order_detail.order.order_details
-    # @order = Order.find(params[:id])
-    #   if @order_details.where(making_status: '製作中').any?
-    #     @order.update(order_status: 2)
-    #   else
-    #     @order_detail.update(order_detail_params)
-    #   end
   end
 
 
